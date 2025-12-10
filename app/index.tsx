2d52ddbe9f9f4eff7b0eb63cb0a580154c8bee2f
saipraveen53 <145278161+saipraveen53@users.noreply.github.com>
2025-12-10 16:48:23 +0530

    First Commit:

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-200">
+      <Text className="text-lg font-semibold text-gray-800">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
