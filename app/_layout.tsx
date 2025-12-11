6145198f4030270678394db5e6e6414212a19b65
saipraveen53 <145278161+saipraveen53@users.noreply.github.com>
2025-12-11 11:06:15 +0530

    Updated:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,26 @@
-import { Stack } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router, Stack } from "expo-router";
+import { jwtDecode } from "jwt-decode";
+import { useEffect } from "react";
 import Context from "./(utils)/Context";
 import './globals.css';
 export default function RootLayout() {
+
+
+useEffect(()=>{
+  let handleRedirectLogic = async()=>{
+    const  Token = await AsyncStorage.getItem("userToken");
+    const decoded = jwtDecode(Token);
+    const Role = decoded.roles[0];
+    if(Role=="ADMIN"){
+      router.replace('/(admin)/Dashboard')
+    }else{
+      router.replace('/(vendor)/Orders')
+    }
+  }
+  handleRedirectLogic();
+})
+
   return (
 
    <Context>
